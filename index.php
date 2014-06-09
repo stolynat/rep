60ece75f4a1864c8eb80f072a7cf33780f3b1620
Nota <natalya.stolyachuk@gmail.com>
2014-06-10 01:46:45 +0300

    number

--- index.php
+++ index.php
@@ -5,13 +5,13 @@
  <body>
  <?php 
 $counter=0;
-$n=5;
+$n=10;
 while ($n>1){
  $n=$n/2;
  $counter=$counter+1;
 }
-
-echo (pow(2, counter) . "<br>");
+echo "The closest number:" . "<br>";
+echo (pow(2, $counter) . "<br>");
 echo "Counter:  $counter";
 
 
